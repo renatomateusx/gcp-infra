711d1d4257427a52df173eb0232be386934cc76a
Renato Santos <renatomateusx@gmail.com>
2025-06-17 09:28:21 -0300

    Trigger workflow

--- lab8-git-actions-gke/infra/main.tf
+++ lab8-git-actions-gke/infra/main.tf
@@ -8,6 +8,7 @@ resource "google_container_cluster" "primary" {
   name               = "gke-cluster"
   location           = var.zone
   initial_node_count = 2
+  deletion_protection = false
 
   node_config {
     machine_type = "e2-medium"
