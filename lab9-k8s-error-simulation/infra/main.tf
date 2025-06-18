7b2e859b7c813a31cbd0b2cae7b8660bb61f5879
Renato Santos <renatomateusx@gmail.com>
2025-06-18 07:50:38 -0300

    Lab 9

--- lab9-k8s-error-simulation/infra/main.tf
+++ lab9-k8s-error-simulation/infra/main.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+  }
+}
+
 # Terraform configuration
 # This will configure the Google Cloud provider
 # The provider will be used to create resources in the Google Cloud platform
@@ -9,6 +23,31 @@ provider "google" {
   project     = var.project_id
   region      = var.region
   zone        = var.zone
+}
+
+# This will configure the Kubernetes provider
+# The provider will be used to create resources in the Kubernetes platform
+# The provider will be configured with the host, token, and cluster CA certificate
+data "google_client_config" "default" {}
+
+# This will configure the Kubernetes provider
+# The provider will be used to create resources in the Kubernetes platform
+# The provider will be configured with the host, token, and cluster CA certificate
+provider "kubernetes" {
+  host  = "https://${google_container_cluster.primary.endpoint}"
+  token = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
+}
+
+# This will configure the Helm provider
+# The provider will be used to create resources in the Helm platform
+# The provider will be configured with the host, token, and cluster CA certificate
+provider "helm" {
+  kubernetes {
+    host  = "https://${google_container_cluster.primary.endpoint}"
+    token = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
+  }
 }
 
 # Create a GKE cluster
@@ -31,6 +70,58 @@ resource "google_container_cluster" "primary" {
   }
 }
 
+# Create a namespace for the Dynatrace operator
+# This will create a namespace for the Dynatrace operator
+# The namespace will be named "dynatrace"
+# The namespace will be created in the GKE cluster
+# The namespace will be created in the zone "us-central1-a"
+# The namespace will be created in the project "able-veld-462218-h4"
+# The namespace will be created in the region "us-central1"
+resource "kubernetes_namespace" "dynatrace" {
+  metadata {
+    name = "dynatrace"
+  }
+}
+
+# This will install the Dynatrace operator
+# The operator will be installed in the namespace "dynatrace"
+# The operator will be installed in the GKE cluster
+# The operator will be installed in the zone "us-central1-a"
+# The operator will be installed in the project "able-veld-462218-h4"
+# The operator will be installed in the region "us-central1"
+resource "helm_release" "dynatrace_operator" {
+  name       = "dynatrace-operator"
+  repository = "https://helm.dynatrace.com"
+  chart      = "dynatrace-operator"
+  namespace  = kubernetes_namespace.dynatrace.metadata[0].name
+  create_namespace = false
+  version    = "0.15.0"
+
+  values = [
+    templatefile("${path.module}/dynatrace-values.tpl.yaml", {
+      dynatrace_url         = var.dynatrace_server,
+      dynatrace_paas_token  = var.dynatrace_paas_token,
+      dynatrace_api_token   = var.dynatrace_api_token
+    })
+  ]
+}
+
+# This will create a Dynakube CRD
+# The CRD will be created in the namespace "dynatrace"
+# The CRD will be created in the GKE cluster
+# The CRD will be created in the zone "us-central1-a"
+# The CRD will be created in the project "able-veld-462218-h4"
+# The CRD will be created in the region "us-central1"
+resource "kubernetes_manifest" "dynakube_crd" {
+  manifest = yamldecode(templatefile("${path.module}/dynakube.tpl.yaml", {
+    dynatrace_url = var.dynatrace_server
+  }))
+
+  depends_on = [helm_release.dynatrace_operator]
+}
+
+
+
 # Variables
 # This will define the variables for the project ID, region, and zone
 # The project ID is the ID of the project in the Google Cloud platform
@@ -45,4 +136,5 @@ variable "region" { }
 variable "zone" { }
 variable "dynatrace_server" { }
 variable "dynatrace_paas_token" { }
+variable "dynatrace_api_token" { }
 
